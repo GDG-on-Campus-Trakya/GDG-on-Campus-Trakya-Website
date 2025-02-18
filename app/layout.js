3ea6e2aca0bf31bc31040cc8ceb14efb98e20a59
Lokman Baturay Efe <65876412+lokicik@users.noreply.github.com>
2025-02-18 17:17:18 +0300

    feat: add metadata

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,24 @@
 // app/layout.js
-import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
-import "./globals.css"
-import AuthProvider from './AuthProvider'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import AuthProvider from "./AuthProvider";
 
+export const metadata = {
+  title: "GDG on Campus Trakya",
+  description: "Google Developer Groups on Campus Trakya University",
+};
 
 export default async function RootLayout({ children }) {
-  
   return (
     <html className="h-full">
       <body className="flex flex-col min-h-screen custom-scrollbar">
-          <AuthProvider>
+        <AuthProvider>
           <Navbar />
-          <main className="flex-1">
-            {children}
-          </main>
+          <main className="flex-1">{children}</main>
           <Footer />
-          </AuthProvider>
+        </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
